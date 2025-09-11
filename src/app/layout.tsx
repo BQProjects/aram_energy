a1294fb0e841f56226095551358c18706c6855cd
Jashkumar Bakul Solanki <jashkumarsolanki@gmail.com>
2025-09-11 14:03:30 +0530

    feat: Remove unused font imports and simplify className in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "./contexts/LanguageContext";
 import { CalculationTarifProvider } from "./contexts/CalculationTarifContext";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Aram Energy Solution",
@@ -27,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <LanguageProvider>
           <CalculationTarifProvider>{children}</CalculationTarifProvider>
